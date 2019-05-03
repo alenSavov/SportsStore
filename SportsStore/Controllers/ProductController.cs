bef22f381974bf7ae44dc615e0c67f0de4bec6a0
alenSavov <playmatte@gmail.com>
2019-05-03 12:50:22 +0300

    Added tests for Cart.cs

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -3,31 +3,35 @@ using SportsStore.Models;
 using System.Linq;
 using SportsStore.Models.ViewModels;
 
-namespace SportsStore.Controllers {
+namespace SportsStore.Controllers
+{
 
-    public class ProductController : Controller {
+    public class ProductController : Controller
+    {
         private IProductRepository repository;
         public int PageSize = 4;
 
-        public ProductController(IProductRepository repo) {
+        public ProductController(IProductRepository repo)
+        {
             repository = repo;
         }
 
         public ViewResult List(string category, int productPage = 1)
-             => View(new ProductsListViewModel
-             {
-                 Products = repository.Products
-             .Where(p => category == null || p.Category == category)
-             .OrderBy(p => p.ProductID)
-             .Skip((productPage - 1) * PageSize)
-             .Take(PageSize),
-                 PagingInfo = new PagingInfo
-                 {
-                     CurrentPage = productPage,
-                     ItemsPerPage = PageSize,
-                     TotalItems = repository.Products.Count()
-                 },
-                 CurrentCategory = category
-             });
+               => View(new ProductsListViewModel
+               {
+                   Products = repository.Products
+               .Where(p => category == null || p.Category == category)
+               .OrderBy(p => p.ProductID)
+               .Skip((productPage - 1) * PageSize)
+               .Take(PageSize),
+                   PagingInfo = new PagingInfo
+                   {
+                       CurrentPage = productPage,
+                       ItemsPerPage = PageSize,
+                       TotalItems = category == null ? repository.Products.Count() : repository.Products.Where(e =>
+                       e.Category == category).Count()
+                   },
+                   CurrentCategory = category
+               });
     }
 }
